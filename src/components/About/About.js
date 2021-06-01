0f6634cd9c5a43f113c843e313f2bee5cd8d937c
BEN <benjamin.hill28@gmail.com>
2021-06-01 09:50:58 -0700

    added nav to app.js, styles to global and MyApp, started About component

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+import styles from './About.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
